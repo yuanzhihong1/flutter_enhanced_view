6c963fe32b53a8b3bf3293f5503ea3e2c77e66e8
袁智鸿 <2524422678@qq.com>
2026-01-27 10:39:41 +0800

    初始化

--- lib/src/view/cupertino_desktop_sheet_dialog.dart
+++ lib/src/view/cupertino_desktop_sheet_dialog.dart
@@ -7,10 +7,13 @@ class CupertinoDesktopSheetDialog extends StatelessWidget {
   final Widget child;
   final Color backgroundColor;
 
+  final CupertinoButtonSize sizeStyle;
+
   const CupertinoDesktopSheetDialog({
     super.key,
     required this.child,
     required this.backgroundColor,
+    this.sizeStyle = CupertinoButtonSize.medium,
   });
 
   @override
@@ -29,6 +32,13 @@ class CupertinoDesktopSheetDialog extends StatelessWidget {
       builder: (context, constraints) {
         double width = constraints.maxWidth / 2.5;
         double height = constraints.maxHeight / 2;
+        if (sizeStyle == CupertinoButtonSize.medium) {
+          height = constraints.maxHeight / 2;
+        } else if (sizeStyle == CupertinoButtonSize.small) {
+          height = constraints.maxHeight / 3;
+        } else if (sizeStyle == CupertinoButtonSize.large) {
+          height = constraints.maxHeight / 1.5;
+        }
         if (backgroundColor.a == 0.0) {
           return Center(
             child: Container(
